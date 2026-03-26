eca4e3d34edc55a71e211844607fb3d99baa2edf
mudkipdev <mudkip@mudkip.dev>
2026-03-26 10:31:06 -0600

    initial commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.1
+import PackageDescription
+
+let package = Package(
+    name: "comet",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.97.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "comet",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio")
+            ]
+        ),
+    ]
+)
